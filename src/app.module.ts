da9ea3b5dde84fc99f6f8107f7369ad2e22813c8
Roman <romamail.ru365@gmail.com>
2025-04-15 23:02:46 +0500

    убрал импорты

--- src/app.module.ts
+++ src/app.module.ts
@@ -5,8 +5,6 @@ import { ConfigModule } from '@nestjs/config';
 import { ProfileModule } from './profile/profile.module';
 import { FilesModule } from './files/files.module';
 import { ProfilePeopleModule } from './profile-people/profile-people.module';
-import { PassportModule } from '@nestjs/passport';
-import { JwtModule } from '@nestjs/jwt';
 import { AccessTokenStrategy } from './strategies/accessToken.strategy';
 import { RefreshTokenStrategy } from './strategies/refreshToken.strategy';
 
