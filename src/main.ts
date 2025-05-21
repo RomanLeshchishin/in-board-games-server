8cdc37920db335e718b8c2e63a0bb61098f08f1c
Roman <romamail.ru365@gmail.com>
2025-05-21 14:45:57 +0500

    изменил main.ts

--- src/main.ts
+++ src/main.ts
@@ -21,7 +21,7 @@ async function bootstrap() {
   app.useGlobalPipes(new ValidationPipe());
 
   app.enableCors({
-    origin: 'http://localhost:3000',
+    origin: 'http://localhost:3001',
     credentials: true,
   });
 
