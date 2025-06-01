f83b338e9414065e247490f4c8a0ef7b0702887d
Roman <romamail.ru365@gmail.com>
2025-06-01 18:16:03 +0500

    исправления ошибок

--- src/chat/chat.controller.ts
+++ src/chat/chat.controller.ts
@@ -1,6 +1,5 @@
 import { Controller, Post, Body, UseGuards, Request, Get, Param } from '@nestjs/common';
 import { ChatService } from './chat.service';
-import { CreateChatDto } from './dto/create-chat.dto';
 import { AccessTokenGuard } from '../guards/accessToken.guard';
 
 @Controller('chats')
@@ -8,11 +7,11 @@ import { AccessTokenGuard } from '../guards/accessToken.guard';
 export class ChatController {
   constructor(private readonly chatService: ChatService) {}
 
-  @Post()
-  async createChat(@Body() dto: CreateChatDto, @Request() req) {
-    const userId = req.user.id;
-    return this.chatService.createChat(userId, dto);
-  }
+  // @Post()
+  // async createChat(@Body() dto: CreateChatDto, @Request() req) {
+  //   const userId = req.user.id;
+  //   return this.chatService.createChat(userId, dto);
+  // }
 
   @Get('/:chatId/messages')
   async getMessages(@Param('chatId') chatId: string) {
--- src/chat/chat.service.ts
+++ src/chat/chat.service.ts
@@ -5,41 +5,41 @@ import { Injectable } from '@nestjs/common';
 export class ChatService {
   constructor(private prisma: PrismaService) {}
 
-  async createChat(userId: string, dto: CreateChatDto) {
-    const chat = await this.prisma.chat.create({
-      data: {
-        title: dto.title,
-        isGroup: dto.isGroup,
-        participants: {
-          create: [
-            ...dto.userIds.map(id => ({
-              user: { connect: { id } },
-            })),
-            {
-              user: { connect: { id: userId } },
-            },
-          ],
-        },
-      },
-      include: { participants: true },
-    });
+  // async createChat(userId: string, dto: CreateChatDto) {
+  //   const chat = await this.prisma.chat.create({
+  //     data: {
+  //       title: dto.title,
+  //       isGroup: dto.isGroup,
+  //       participants: {
+  //         create: [
+  //           ...dto.userIds.map(id => ({
+  //             user: { connect: { id } },
+  //           })),
+  //           {
+  //             user: { connect: { id: userId } },
+  //           },
+  //         ],
+  //       },
+  //     },
+  //     include: { participants: true },
+  //   });
+  //
+  //   return chat;
+  // }
 
-    return chat;
-  }
-
-  async sendMessage(userId: string, dto: SendMessageDto) {
-    const message = await this.prisma.message.create({
-      data: {
-        chatId: dto.chatId,
-        senderId: userId,
-        content: dto.content,
-        fileId: dto.fileId || null,
-      },
-      include: { sender: true },
-    });
-
-    return message;
-  }
+  // async sendMessage(userId: string, dto: SendMessageDto) {
+  //   const message = await this.prisma.message.create({
+  //     data: {
+  //       chatId: dto.chatId,
+  //       senderId: userId,
+  //       content: dto.content,
+  //       fileId: dto.fileId || null,
+  //     },
+  //     include: { sender: true },
+  //   });
+  //
+  //   return message;
+  // }
 
   async getMessages(chatId: string) {
     return this.prisma.message.findMany({
